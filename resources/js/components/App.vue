7a8620b9f77ec6cf0d767c86408f9b10068cecbc
Maciej Wisniewski <maciej.wisniewski@aol.com>
2025-04-07 21:49:25 +0200

    Add Vue.js integration to replace Tailwind in Vite config
    - Replace tailwindcss plugin with Vue plugin in vite.config.js
    - Add package-lock.json with Vue dependencies
    - Create basic App.vue component in resources/js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        <h1>Witaj w Giełdzie Samochodowej</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+/* Twoje style */
+</style>
